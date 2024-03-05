905707739faa71c9ecc1fecf00e6aae7433805d7
Minh Trong <traminhtrong2604@gmail.com>
2024-03-05 20:47:22 +0700

    Add minting and sending functionality

--- x/tokenfactory/keeper/msg_server_update_owner.go
+++ x/tokenfactory/keeper/msg_server_update_owner.go
@@ -5,14 +5,44 @@ import (
 
 	"tokenfactory/x/tokenfactory/types"
 
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) UpdateOwner(goCtx context.Context, msg *types.MsgUpdateOwner) (*types.MsgUpdateOwnerResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Check if the value exist
+	valFound, isFound := k.GetDenom(
+		ctx,
+		msg.Denom,
+	)
+	if !isFound {
+		return nil, errorsmod.Wrap(sdkerrors.ErrKeyNotFound, "denom does not exist")
+	}
+
+	// Check if the msg owner is the same as the current owner
+	if msg.Owner != valFound.Owner {
+		return nil, errorsmod.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+
+	var denom = types.Denom{
+		Owner:              msg.NewOwner,
+		Denom:              msg.Denom,
+		Description:        valFound.Description,
+		MaxSupply:          valFound.MaxSupply,
+		Supply:             valFound.Supply,
+		Precision:          valFound.Precision,
+		Ticker:             valFound.Ticker,
+		Url:                valFound.Url,
+		CanChangeMaxSupply: valFound.CanChangeMaxSupply,
+	}
+
+	k.SetDenom(
+		ctx,
+		denom,
+	)
 
 	return &types.MsgUpdateOwnerResponse{}, nil
 }
