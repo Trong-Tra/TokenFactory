367eea0b3817e3fbd6b3bc0b9968061037fd46a9
Minh Trong <traminhtrong2604@gmail.com>
2024-03-05 16:53:06 +0700

    build: Add token factory create and update logic

--- x/tokenfactory/types/expected_keepers.go
+++ x/tokenfactory/types/expected_keepers.go
@@ -4,18 +4,19 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
-// AccountKeeper defines the expected interface for the Account module.
 type AccountKeeper interface {
-	GetAccount(context.Context, sdk.AccAddress) sdk.AccountI // only used for simulation
-	// Methods imported from account should be defined here
+	GetAccount(ctx sdk.Context, addr sdk.AccAddress) authtypes.AccountI
+	GetModuleAddress(name string) sdk.AccAddress
+	GetModuleAccount(ctx sdk.Context, moduleName string) authtypes.ModuleAccountI
 }
 
-// BankKeeper defines the expected interface for the Bank module.
 type BankKeeper interface {
+	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
+	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
 	SpendableCoins(context.Context, sdk.AccAddress) sdk.Coins
-	// Methods imported from bank should be defined here
 }
 
 // ParamSubspace defines the expected Subspace interface for parameters.
